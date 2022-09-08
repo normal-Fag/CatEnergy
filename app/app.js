70f6a37bd24ddce5d39c53f7fbfd226df6bb6dfe
AK <72517164+normal-Fag@users.noreply.github.com>
2022-09-08 15:58:16 +0700

    Cat Energy

---
+++ app/app.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    document.querySelector('.header__burger').addEventListener('click', onBurgerClick);
+
+    document.querySelector('.comparison__input').addEventListener('input', handleInputChange);
+}
+
+const onBurgerClick = () => {
+    document.querySelector('.header').classList.toggle('header--active');
+}
+
+const handleInputChange = (e) => {
+    console.log('Input');
+    let target = e.target;
+    const min = target.min;
+    const max = target.max;
+    const value = target.value;
+
+    target.style.backgroundSize = (value - min) * 100 / (max - min) + '% 100%';
+}
